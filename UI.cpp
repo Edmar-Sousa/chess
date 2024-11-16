a2ec7eb0c48a419a28f1d0d27f0d5d9d7ee3734d
Edmar-Sousa <edmarsousamendes4@gmail.com>
2024-11-16 15:29:41 -0300

    feat: colors to tiles from board

--- UI.cpp
+++ UI.cpp
@@ -1,9 +1,13 @@
 #include "UI.hpp"
+#include "Board.hpp"
 #include "Constants.hpp"
 #include <raylib.h>
 
 UI::UI(Board board) {
   this->board = board;
+
+  blackHouse = {183, 192, 216, 255};
+  whiteHouse = {232, 237, 249, 255};
 
   std::cout << "WINDOW: " << WINDOW_WIDTH << 'x' << WINDOW_HEIGHT << std::endl;
 
@@ -20,10 +24,25 @@ void UI::drawBoard() {
     BeginDrawing();
     ClearBackground(RAYWHITE);
 
-    DrawText("Basic Window", BOARD_LEFT_PADDING, 0, 20, LIGHTGRAY);
+    drawTiles();
+
     EndDrawing();
     // End Draw
   }
 }
 
-void UI::drawTiles() {}
+void UI::drawTiles() {
+  int *table = board.getBoard();
+  int index = 0;
+
+  Color house;
+
+  for (int col = 0; col < COLS; col++) {
+    for (int row = 0; row < ROWS; row++) {
+      house = table[index++] ? whiteHouse : blackHouse;
+
+      DrawRectangle(col * BOARD_TILE_SIZE, row * BOARD_TILE_SIZE,
+                    BOARD_TILE_SIZE, BOARD_TILE_SIZE, house);
+    }
+  }
+}
