1ee8c96d902878f8a1dc352b1677b147d9d7c03d
Edmar-Sousa <edmarsousamendes4@gmail.com>
2024-11-16 14:55:28 -0300

    feat: draw a basic window

--- UI.cpp
+++ UI.cpp
@@ -1,6 +1,29 @@
 #include "UI.hpp"
+#include "Constants.hpp"
+#include <raylib.h>
 
-void UI::drawBoard() 
-{
-    
+UI::UI(Board board) {
+  this->board = board;
+
+  std::cout << "WINDOW: " << WINDOW_WIDTH << 'x' << WINDOW_HEIGHT << std::endl;
+
+  InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Chess Engine");
+  SetTargetFPS(FPS);
 }
+
+UI::~UI() { CloseWindow(); }
+
+void UI::drawBoard() {
+  while (!WindowShouldClose()) {
+
+    // Draw
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+
+    DrawText("Basic Window", BOARD_LEFT_PADDING, 0, 20, LIGHTGRAY);
+    EndDrawing();
+    // End Draw
+  }
+}
+
+void UI::drawTiles() {}
