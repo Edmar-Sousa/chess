c92041798d2e9793a29ad5545207e27cf1a83a4c
Edmar-Sousa <edmarsousamendes4@gmail.com>
2024-11-16 22:56:02 -0300

    feat: draw pieces

--- UI.cpp
+++ UI.cpp
@@ -1,8 +1,8 @@
 #include "UI.hpp"
 #include "Board.hpp"
 #include "Constants.hpp"
+#include <cstdlib>
 #include <raylib.h>
-#include <string>
 
 UI::UI(Board board) {
   this->board = board;
@@ -12,10 +12,15 @@ UI::UI(Board board) {
   labelColor = {52, 54, 76, 255};
 
   InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Chess Engine");
+
+  piecesTexture = LoadTexture("./assets/pieces.png");
   SetTargetFPS(FPS);
 }
 
-UI::~UI() { CloseWindow(); }
+UI::~UI() { 
+  UnloadTexture(piecesTexture);
+  CloseWindow();
+}
 
 void UI::drawBoard() {
   while (!WindowShouldClose()) {
@@ -33,13 +38,15 @@ void UI::drawBoard() {
 
 void UI::drawTiles() {
   int *table = board.getBoard();
+  int *pieces = board.getPieces();
+
   int index = 0;
 
   Color house;
 
-  for (int col = 0; col < COLS; col++) {
-    for (int row = 0; row < ROWS; row++) {
-      house = table[index++] ? whiteHouse : blackHouse;
+  for (int row = 0; row < ROWS; row++) {
+    for (int col = 0; col < COLS; col++) {
+      house = table[index] ? whiteHouse : blackHouse;
 
       DrawText(std::to_string(8 - col).c_str(), BOARD_LEFT_PADDING / 2,
                POSITION_TILE(col, BOARD_TILE_SIZE / 2), BOARD_LABEL_SIZE,
@@ -56,6 +63,45 @@ void UI::drawTiles() {
       DrawRectangle(POSITION_TILE(col, BOARD_LEFT_PADDING),
                     POSITION_TILE(row, BOARD_TOP_PADDING), BOARD_TILE_SIZE,
                     BOARD_TILE_SIZE, house);
+
+      drawPiece(col, row, pieces[index]);
+      index += 1;
     }
   }
+}
+
+void UI::drawPiece(int col, int row, int piece)
+{
+  float posx = 0.0;
+  float posy = 0.0;
+
+  switch (piece) {
+    case Pieces::BLACK_PAWN:
+    case Pieces::BLACK_ROOK:
+    case Pieces::BLACK_KNIGHT:
+    case Pieces::BLACK_BISHOP:
+    case Pieces::BLACK_KING:
+    case Pieces::BLACK_QUEEN:
+      posx = (float) BOARD_TILE_SIZE * piece;
+      posy = 0.0;
+      break;
+
+    case Pieces::WHITE_PAWN:
+    case Pieces::WHITE_ROOK:
+    case Pieces::WHITE_KNIGHT:
+    case Pieces::WHITE_BISHOP:
+    case Pieces::WHITE_KING:
+    case Pieces::WHITE_QUEEN:
+      posx = (float) BOARD_TILE_SIZE * (piece - 6);
+      posy = BOARD_TILE_SIZE;
+      break;
+
+    case Pieces::EMPTY:
+      return;
+  }
+
+  Rectangle source = { posx, posy, BOARD_TILE_SIZE, BOARD_TILE_SIZE };
+  Vector2 position = { (float) col * BOARD_TILE_SIZE + BOARD_LEFT_PADDING, (float) row * BOARD_TILE_SIZE + BOARD_TOP_PADDING };
+
+  DrawTextureRec(piecesTexture, source, position, WHITE);
 }
