cad9da6433f50cc886f678e144edaebfc7d562dd
Edmar-Sousa <edmarsousamendes4@gmail.com>
2024-11-16 21:38:25 -0300

    fix: change constants ui to UI header

--- Constants.hpp
+++ Constants.hpp
@@ -4,20 +4,8 @@
 #include <iostream>
 #include <raylib.h>
 
-
-#define COLS               8
-#define ROWS               8
-
-#define BOARD_TILE_SIZE       72
-
-#define BOARD_TOP_PADDING     12
-#define BOARD_BOTTOM_PADDING  48
-#define BOARD_LEFT_PADDING    48
-#define BOARD_RIGHT_PADDING   12
-
-
-#define WINDOW_WIDTH   BOARD_LEFT_PADDING + (BOARD_TILE_SIZE * COLS) + BOARD_RIGHT_PADDING
-#define WINDOW_HEIGHT  BOARD_TOP_PADDING + (BOARD_TILE_SIZE * ROWS) + BOARD_BOTTOM_PADDING
+#define COLS 8
+#define ROWS 8
 
 typedef unsigned long long U64;
 
--- UI.hpp
+++ UI.hpp
@@ -7,11 +7,26 @@
 #define FPS 60
 #define POSITION_TILE(pos, padding) pos *BOARD_TILE_SIZE + padding
 
+#define BOARD_LABEL_SIZE 18
+
+#define BOARD_TILE_SIZE 72
+
+#define BOARD_TOP_PADDING 12
+#define BOARD_BOTTOM_PADDING 48
+#define BOARD_LEFT_PADDING 48
+#define BOARD_RIGHT_PADDING 12
+
+#define WINDOW_WIDTH                                                           \
+  BOARD_LEFT_PADDING + (BOARD_TILE_SIZE * COLS) + BOARD_RIGHT_PADDING
+#define WINDOW_HEIGHT                                                          \
+  BOARD_TOP_PADDING + (BOARD_TILE_SIZE * ROWS) + BOARD_BOTTOM_PADDING
+
 class UI {
   Board board;
 
   Color blackHouse;
   Color whiteHouse;
+  Color labelColor;
 
   void drawTiles();
 
