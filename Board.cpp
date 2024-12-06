9736f684f54cb611b09574ba487fa8faf7109b75
Edmar-Sousa <edmarsousamendes4@gmail.com>
2024-12-05 21:04:11 -0300

    style: change format

--- Board.cpp
+++ Board.cpp
@@ -23,18 +23,14 @@ void Board::draw() {
   std::cout << "\n";
 
   for (int index = 0; index < COLS * ROWS; index++)
-    std::cout << ' ' << boardColors[index]
-              << ((index + 1) % ROWS == 0 ? '\n' : ' ');
+    std::cout << ' ' << boardColors[index] << ((index + 1) % ROWS == 0 ? '\n' : ' ');
 
   std::cout << "\n";
 }
 
 int *Board::getBoard() { return this->boardColors; }
 
-uint64_t Board::getPieces() {
-  return pawnsWhite & rooksWhite & knightsWhite & bishopsWhite & kingsWhite &
-         queensWhite;
-}
+uint64_t Board::getPieces() { return pawnsWhite & rooksWhite & knightsWhite & bishopsWhite & kingsWhite & queensWhite; }
 
 uint64_t Board::getPawns() { return pawnsWhite; }
 uint64_t Board::getRooks() { return rooksWhite; }
