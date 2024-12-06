a99a209b07806f66ddbe80fc1671c6747afe3277
Edmar-Sousa <edmarsousamendes4@gmail.com>
2024-12-05 21:40:24 -0300

    fix: init bitboard black pieces

--- Board.cpp
+++ Board.cpp
@@ -18,12 +18,12 @@ Board::Board() {
   queensWhite = 0x0000000000000008;
   kingsWhite = 0x0000000000000010;
 
-  pawnsWhite = 0x00FF000000000000;
-  rooksWhite = 0x8100000000000000;
-  knightsWhite = 0x4200000000000000;
-  bishopsWhite = 0x2400000000000000;
-  queensWhite = 0x0800000000000000;
-  kingsWhite = 0x1000000000000000;
+  pawnsBlack = 0x00FF000000000000;
+  rooksBlack = 0x8100000000000000;
+  knightsBlack = 0x4200000000000000;
+  bishopsBlack = 0x2400000000000000;
+  queensBlack = 0x0800000000000000;
+  kingsBlack = 0x1000000000000000;
 }
 
 void Board::draw() {
